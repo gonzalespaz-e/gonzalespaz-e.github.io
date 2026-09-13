f905ec00749947335d753bae49e6e75dbd57865b
gonzalespaz-e <emmanuel.gp.82@gmail.com>
2026-09-13 19:11:09 -0400

    documenting logic + various solutions for functionality

--- csce242/assignments/05js/scripts/05.js
+++ csce242/assignments/05js/scripts/05.js
@@ -1,12 +1,15 @@
+// Making bubbel and tail simulatenously visible onclick using hidden class toggle
 document.getElementById("speech-section").onclick = () => {
     document.getElementById("bubble").classList.toggle("hidden");
     document.getElementById("tail").classList.toggle("hidden");
 };
 
+// Using selection on change to display the selection option + the text
 document.getElementById("drinks").onchange = (e) => {
     document.getElementById("drink-selection").innerHTML = e.target.value + ": Nice choice!";
 };
 
+// Adding absolutely positioned emoji on click by toggling hidden status
 document.getElementById("card-img").onclick = () => {
     document.getElementById("sticker-emoji").classList.toggle("hidden");
 }
